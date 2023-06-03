13900aaa4291fa7fff90b998865ac3596c916f23
Karduk8000 <134720038+Karduk8000@users.noreply.github.com>
2023-06-03 18:03:19 +0200

    Update main.py
    добавлен отскок мячика от стенок и ракеток, добавлено ускорение меча с каждым отскоком, добавлены надписи проигрыша обоих игроков

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class Player(GameSprite):
             self.rect.y -= self.speed
 
         # если нажата клавиша вправо и физическая модель не ушла за правую границу игры
-        if keys[K_DOWN] and self.rect.y < width - 70:
+        if keys[K_DOWN] and self.rect.y < height - 150:
             self.rect.y += self.speed
 
     def update_l(self):
@@ -51,7 +51,7 @@ class Player(GameSprite):
             self.rect.y -= self.speed
 
         # если нажата клавиша вправо и физическая модель не ушла за правую границу игры
-        if keys[K_s] and self.rect.y < width - 70:
+        if keys[K_s] and self.rect.y < height - 150:
             self.rect.y += self.speed
 
 # переменная окончания игры
@@ -60,7 +60,7 @@ finish = False  # когда True, то спрайты перестают раб
 game = True  # завершается при нажатии кнопки закрыть окно
 
 # размеры окна
-width = 700
+width = 600
 height = 500
 
 # создание окна
@@ -74,8 +74,8 @@ FPS = 60
 # шрифт
 font.init()
 font1 = font.SysFont("Arial", 36)
-lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
-lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+lose1 = font1.render('левый попуск', True, (180, 0, 0))
+lose2 = font1.render('правый ботик', True, (180, 0, 0))
 
 racket1 = Player('racket.png', 30, 200, 4, 50, 150)
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
@@ -97,49 +97,41 @@ while game:
         ball.rect.x += ball_x
         ball.rect.y += ball_y
 
+        #проверка на отскок ракетки
+        if sprite.collide_rect(racket1, ball):
+            ball_x *= -1
+            ball_x += 0.1
+            if ball_y > 0:
+                ball_y += 1
+            else:
+                ball_y -= 1
+
+        if sprite.collide_rect(racket2, ball):
+            ball_x *= -1
+            ball_x -= 0.1
+            if ball_y > 0:
+                ball_y += 1
+            else:
+                ball_y -= 1
+
+    #ПРОИГРЫШ
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+
+        if ball.rect.x > width - 50:
+            finish = True
+            window.blit(lose2, (200, 200))
+
+        #отскок от пола или потолка
+        if ball.rect.y < 0 or ball.rect.y > height - 50:
+            ball_y *= -1
+
+
+
         racket1.reset()
         racket2.reset()
         ball.reset()
 
     display.update()
     clock.tick(FPS)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
